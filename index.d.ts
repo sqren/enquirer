74789e30783c3ce43652ab89bc6ec76216c3d31d
Søren Louv-Jansen <sorenlouv@gmail.com>
2020-05-07 11:48:03 +0200

    upddated types

--- index.d.ts
+++ index.d.ts
@@ -1,104 +1,126 @@
 import { EventEmitter } from "events";
 
 interface BasePromptOptions {
-  name: string | (() => string)
-  type: string | (() => string)
-  message: string | (() => string) | (() => Promise<string>)
-  initial?: any
-  required?: boolean
-  format?(value: string): string | Promise<string>
-  result?(value: string): string | Promise<string>
-  skip?: ((state: object) => boolean | Promise<boolean>) | boolean
-  validate?(value: string): boolean | Promise<boolean> | string | Promise<string>
-  onSubmit?(name: string, value: any, prompt: Enquirer.Prompt): boolean | Promise<boolean>
-  onCancel?(name: string, value: any, prompt: Enquirer.Prompt): boolean | Promise<boolean>
-  stdin?: NodeJS.ReadStream
-  stdout?: NodeJS.WriteStream
+  name: string | (() => string);
+  type: string | (() => string);
+  message: string | (() => string) | (() => Promise<string>);
+  initial?: any;
+  required?: boolean;
+  format?(value: string): string | Promise<string>;
+  result?(value: string): string | Promise<string>;
+  skip?: ((state: object) => boolean | Promise<boolean>) | boolean;
+  validate?(
+    value?: string
+  ): boolean | Promise<boolean> | string | Promise<string>;
+  onSubmit?(
+    name: string,
+    value: any,
+    prompt: Enquirer.Prompt
+  ): boolean | Promise<boolean>;
+  onCancel?(
+    name: string,
+    value: any,
+    prompt: Enquirer.Prompt
+  ): boolean | Promise<boolean>;
+  stdin?: NodeJS.ReadStream;
+  stdout?: NodeJS.WriteStream;
 }
 
 interface Choice {
-  name: string
-  message?: string
-  value?: string
-  hint?: string
-  disabled?: boolean | string
+  name: string;
+  message?: string;
+  value?: string;
+  hint?: string;
+  disabled?: boolean | string;
 }
 
 interface ArrayPromptOptions extends BasePromptOptions {
   type:
-    | 'autocomplete'
-    | 'editable'
-    | 'form'
-    | 'multiselect'
-    | 'select'
-    | 'survey'
-    | 'list'
-    | 'scale'
-  choices: string[] | Choice[]
-  maxChoices?: number
-  muliple?: boolean
-  initial?: number
-  delay?: number
-  separator?: boolean
-  sort?: boolean
-  linebreak?: boolean
-  edgeLength?: number
-  align?: 'left' | 'right'
-  scroll?: boolean
+    | "autocomplete"
+    | "editable"
+    | "form"
+    | "multiselect"
+    | "select"
+    | "survey"
+    | "list"
+    | "scale";
+  choices: string[] | Choice[];
+  maxChoices?: number;
+  muliple?: boolean;
+  initial?: number;
+  delay?: number;
+  separator?: boolean;
+  sort?: boolean;
+  linebreak?: boolean;
+  edgeLength?: number;
+  align?: "left" | "right";
+  scroll?: boolean;
 }
 
 interface BooleanPromptOptions extends BasePromptOptions {
-  type: 'confirm'
-  initial?: boolean
+  type: "confirm";
+  initial?: boolean;
 }
 
 interface StringPromptOptions extends BasePromptOptions {
-  type: 'input' | 'invisible' | 'list' | 'password' | 'text'
-  initial?: string
-  multiline?: boolean
+  type: "input" | "invisible" | "list" | "password" | "text";
+  initial?: string;
+  multiline?: boolean;
 }
 
 interface NumberPromptOptions extends BasePromptOptions {
-  type: 'numeral'
-  min?: number
-  max?: number
-  delay?: number
-  float?: boolean
-  round?: boolean
-  major?: number
-  minor?: number
-  initial?: number
+  type: "numeral";
+  min?: number;
+  max?: number;
+  delay?: number;
+  float?: boolean;
+  round?: boolean;
+  major?: number;
+  minor?: number;
+  initial?: number;
 }
 
 interface SnippetPromptOptions extends BasePromptOptions {
-  type: 'snippet'
-  newline?: string
-  template?: string
+  type: "snippet";
+  newline?: string;
+  template?: string;
 }
 
 interface SortPromptOptions extends BasePromptOptions {
-  type: 'sort'
-  hint?: string
-  drag?: boolean
-  numbered?: boolean
+  type: "sort";
+  hint?: string;
+  drag?: boolean;
+  numbered?: boolean;
 }
 
-type PromptOptions =
+export type PromptOptions =
   | BasePromptOptions
   | ArrayPromptOptions
   | BooleanPromptOptions
   | StringPromptOptions
   | NumberPromptOptions
   | SnippetPromptOptions
-  | SortPromptOptions
+  | SortPromptOptions;
+
+interface State {
+  size: number;
+  submitted: boolean;
+}
 
 declare class BasePrompt extends EventEmitter {
-    constructor(options?: PromptOptions);
+  cursorHide: () => void;
+  message: () => string;
+  prefix: () => string;
+  clear: (num: number) => void;
+  write: (value: string) => void;
+  restore: () => void;
+  state: State;
+  constructor(options?: PromptOptions);
 
-    render(): void;
+  render(): void;
 
-    run(): Promise<any>;
-  }
+  run(): Promise<any>;
+}
 
 declare class Enquirer<T = object> extends EventEmitter {
   constructor(options?: object, answers?: T);
@@ -109,12 +131,17 @@ declare class Enquirer<T = object> extends EventEmitter {
    * @param type
    * @param fn `Prompt` class, or a function that returns a `Prompt` class.
    */
-  register(type: string, fn: typeof BasePrompt | (() => typeof BasePrompt)): this;
+  register(
+    type: string,
+    fn: typeof BasePrompt | (() => typeof BasePrompt)
+  ): this;
 
   /**
    * Register a custom prompt type.
    */
-  register(type: { [key: string]: typeof BasePrompt | (() => typeof BasePrompt) }): this;
+  register(type: {
+    [key: string]: typeof BasePrompt | (() => typeof BasePrompt);
+  }): this;
 
   /**
    * Prompt function that takes a "question" object or array of question objects,
